b1a4e1f6c79de9105817a47eb25d4b57265ffac2
Tim Steinberger <tsteinb@protonmail.com>
2021-04-21 20:37:56 -0400

    Added champions helper functions

--- src/champions.rs
+++ src/champions.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::pedantic)]
 
+#[derive(PartialEq, Eq, Clone)]
 enum Champions {
     Androxus,
     Ash,
@@ -51,6 +52,7 @@ enum Champions {
     Zhin,
 }
 
+#[derive(PartialEq, Eq, Clone)]
 enum Class {
     Damage,
     Flank,
@@ -109,6 +111,91 @@ impl Champions {
             | Champions::Talus
             | Champions::Vora
             | Champions::Zhin => Class::Flank,
+        }
+    }
+}
+
+impl Champions {
+    fn uses_abilities_frequently(&self) -> bool {
+        match self {
+            Champions::Viktor | Champions::Yagorath | Champions::Io | Champions::Strix => false,
+            _ => true,
+        }
+    }
+
+    fn has_major_ult(&self) -> bool {
+        match self {
+            Champions::Viktor
+            | Champions::Ying
+            | Champions::Grohk
+            | Champions::Corvus
+            | Champions::Pip
+            | Champions::Imani => true,
+            _ => false,
+        }
+    }
+
+    fn needs_healer(&self) -> bool {
+        if self.get_class() == Class::FrontLine {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn has_sustained_fire(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn is_blaster(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn high_dps(&self) -> bool {
+        if self.get_class() == Class::Damage || self.get_class() == Class::Flank {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn has_normal_shield(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn lacks_mobility(&self) -> bool {
+        match self {
+            Champions::Tyra | Champions::Terminus => true,
+            _ => false,
+        }
+    }
+
+    fn has_aoe(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn has_deployable(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn has_special_or_slow_reload(&self) -> bool {
+        match self {
+            Champions::Damba | Champions::Dredge | Champions::Pip => true,
+            _ => false,
+        }
+    }
+
+    fn is_squishy(&self) -> bool {
+        match self {
+            Champions::Evie | Champions::Maeve => true,
+            _ => false,
+        }
+    }
+
+    fn is_fast(&self) -> bool {
+        match self {
+            Champions::Io | Champions::Talus => true,
+            _ => false,
         }
     }
 }
